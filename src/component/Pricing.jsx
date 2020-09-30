eea8c6d26b7bfbd988afeeea325873c647f9b017
Diwas Bhandari <53388038+Diwas2055@users.noreply.github.com>
2020-09-30 14:47:51 +0545

    Update Pricing.jsx

--- src/component/Pricing.jsx
+++ src/component/Pricing.jsx
@@ -16,14 +16,14 @@ export class Pricing extends Component {
             <h6 className="card-price text-center">$0<span className="period">/month</span></h6>
             <hr />
             <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Single User</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>5GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited Private Projects</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated Phone Support</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Free Subdomain</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status Reports</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Single User</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>5GB Storage</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Community Access</li>
+              <li className="text-muted"><span className="fa-li"><i className="fa fa-times"></i></span>Unlimited Private Projects</li>
+              <li className="text-muted"><span className="fa-li"><i className="fa fa-times"></i></span>Dedicated Phone Support</li>
+              <li className="text-muted"><span className="fa-li"><i className="fa fa-times"></i></span>Free Subdomain</li>
+              <li className="text-muted"><span className="fa-li"><i className="fa fa-times"></i></span>Monthly Status Reports</li>
             </ul>
             <Link to="/" className="btn btn-block btn-success text-uppercase">Button</Link>
           </div>
@@ -37,14 +37,14 @@ export class Pricing extends Component {
             <h6 className="card-price text-center">$9<span className="period">/month</span></h6>
             <hr />
             <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>5 Users</strong></li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>50GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Free Subdomain</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status Reports</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span><strong>5 Users</strong></li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>50GB Storage</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Community Access</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Unlimited Private Projects</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Dedicated Phone Support</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Free Subdomain</li>
+              <li className="text-muted"><span className="fa-li"><i className="fa fa-times"></i></span>Monthly Status Reports</li>
             </ul>
             <Link to="#" className="btn btn-block btn-success  text-uppercase">Button</Link>
           </div>
@@ -58,14 +58,14 @@ export class Pricing extends Component {
             <h6 className="card-price text-center">$49<span className="period">/month</span></h6>
             <hr />
             <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited Users</strong></li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>150GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited</strong> Free Subdomains</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Monthly Status Reports</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span><strong>Unlimited Users</strong></li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>150GB Storage</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Community Access</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Unlimited Private Projects</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Dedicated Phone Support</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span><strong>Unlimited</strong> Free Subdomains</li>
+              <li><span className="fa-li"><i className="fa fa-check"></i></span>Monthly Status Reports</li>
             </ul>
             <Link to="#" className="btn btn-block btn-success  text-uppercase">Button</Link>
           </div>
